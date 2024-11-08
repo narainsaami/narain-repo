8d8e29daefe3b4c72a8ddb76591f95accc16745b
Narayanasamy Seetharaman <narainsaami@gmail.com>
2024-11-08 22:59:11 +0530

    Update fastapi_practice.py

--- fastapi_practice.py
+++ fastapi_practice.py
@@ -1,12 +1,17 @@
-# main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello"}
+# Serve static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/data")
-async def hello():
-    return {"message": "Hello! This is something."}
+# Set up templates
+templates = Jinja2Templates(directory="templates")
+
+# Homepage route
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
